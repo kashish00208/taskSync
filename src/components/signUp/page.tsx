1fc5176a2e9eeac985e37cbfa9b7574d534f00d7
kashish00208 <kgupta7827@gmail.com>
2025-03-06 17:02:14 +0530

    signup-UI

---
+++ src/components/signUp/page.tsx
@@ -0,0 +1,25 @@
+"use client";
+import React from "react";
+
+const page = () => {
+  const handleinputchange = async (e: React.FormEvent) => {
+    
+  };
+  return (
+    <div>
+      <div>
+        <form onSubmit={handleinputchange}>
+          <div>
+            <input type="email" />
+            <input type="text" />
+            <input type="text" />
+            <input type="text" />
+          </div>
+          <button type="submit"></button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default page;
