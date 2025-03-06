af750e96ce5fce43bd7158049b2b0437869c67b3
kashish00208 <kgupta7827@gmail.com>
2025-03-06 14:11:29 +0530

    creating tokens using jsonwebtoken

--- src/lib/authutils.ts
+++ src/lib/authutils.ts
@@ -1,1 +1,16 @@
 import bcrypt from 'bcryptjs'
+import jwt from 'jsonwebtoken'
+
+export const hashedPasswoed = async(password:string) => {
+    const salt = await bcrypt.genSalt(10);
+    return bcrypt.hash(password,salt)
+} 
+
+export const comparePass = async(password:string,hashedPasswoed:string) =>{
+    return bcrypt.compare(password,hashedPasswoed)
+}
+
+//creating jsonwebtoken
+export const createToken = (userID:string) =>{
+    return jwt.sign({userID},process.env.JWT_KEY,{expiresin:'1h'});
+} 
