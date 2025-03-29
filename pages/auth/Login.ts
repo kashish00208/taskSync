a0d4ebaea6178c79a4c374d0a561693e7885fa0b
kashish00208 <kgupta7827@gmail.com>
2025-03-29 22:58:52 +0530

    better post request

--- pages/auth/Login.ts
+++ pages/auth/Login.ts
@@ -1,56 +1,50 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-import dbConnect from '../lib/dbConnect';
+import { NextApiRequest, NextApiResponse } from "next";
+import dbConnect from "../lib/dbConnect";
 import User from "../models/User";
-import bcrypt from 'bcryptjs';  
-import jwt from 'jsonwebtoken';  
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 const login = async (req: NextApiRequest, res: NextApiResponse) => {
-    const {  email, password} = req.body;
+  const { email, password } = req.body;
 
-    if (req.method === 'POST') {
-        if ( !email || !password ) {
-            return res.status(400).json({ message: "All fields are required" });
-        }
+  if (!email || !password) {
+    return res.status(400).json({ message: "All fields are required" });
+  }
 
-        try {
-            await dbConnect();
+  try {
+    await dbConnect();
 
-            const existingUser = await User.findOne({ email });
-            
-            if (!existingUser) {
-                return res.status(404).json({ message: "User not found" });
-            }
+    const existingUser = await User.findOne({ email });
 
-            // password matching
-            const passwordMatch = await bcrypt.compare(password, existingUser.password);
-
-            if (!passwordMatch) {
-                return res.status(401).json({ message: "Invalid credentials" });
-            }
-
-            const token = jwt.sign(
-                { id: existingUser._id, email: existingUser.email},
-                process.env.JWT_SECRET!,  
-                { expiresIn: '1h' }       
-            );
-
-            return res.status(200).json({
-                message: "Login successful",
-                token,
-                user: {
-                    id: existingUser._id,
-                    email: existingUser.email,
-                    username: existingUser.username,
-                }
-            });
-
-        } catch (error) {
-            console.error("Error during login", error);
-            return res.status(500).json({ message: "Internal server error" });
-        }
-    } else {
-        console.error("Method not allowed, use POST request here");
-        return res.status(405).json({ message: "Method not allowed" });
+    if (!existingUser) {
+      return res.status(404).json({ message: "User not found" });
     }
+
+    // password matching
+    const passwordMatch = await bcrypt.compare(password, existingUser.password);
+
+    if (!passwordMatch) {
+      return res.status(401).json({ message: "Invalid credentials" });
+    }
+
+    const token = jwt.sign(
+      { id: existingUser._id, email: existingUser.email },
+      process.env.JWT_SECRET!,
+      { expiresIn: "1h" }
+    );
+
+    return res.status(200).json({
+      message: "Login successful",
+      token,
+      user: {
+        id: existingUser._id,
+        email: existingUser.email,
+        username: existingUser.username,
+      },
+    });
+  } catch (error) {
+    console.error("Error during login", error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
 };
 
 export default login;
--- pages/auth/SignUp.ts
+++ pages/auth/SignUp.ts
@@ -1,49 +1,53 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import User from "../models/User";
 import dbConnect from "../lib/dbConnect";
-import { hashPassword, createToken } from '../lib/authutils';
+import { hashPassword, createToken } from "../lib/authutils";
 
 const signUPHandler = async (req: NextApiRequest, res: NextApiResponse) => {
-  if (req.method === 'POST') {
-    const { email, username, password, role } = req.body;
+  const { email, username, password, role } = req.body;
 
-    // Checking if all fields are provided
-    if (!email || !username || !password || !role) {
-      return res.status(400).json({ message: "Please provide all fields." });
+  // Checking if all fields are provided
+  if (!email || !username || !password || !role) {
+    return res.status(400).json({ message: "Please provide all fields." });
+  }
+
+  try {
+    // Connect database
+    await dbConnect();
+
+    // Check if the email already exists
+    const existingUser = await User.findOne({ email: email });
+    if (existingUser) {
+      return res
+        .status(400)
+        .json({ message: "Email already exists. Try another email." });
     }
 
-    try {
-      // Connect database
-      await dbConnect();
+    // Hash the password
+    const hashedPassword = await hashPassword(password);
 
-      // Check if the email already exists
-      const existingUser = await User.findOne({ email: email });
-      if (existingUser) {
-        return res.status(400).json({ message: "Email already exists. Try another email." });
-      }
+    // Create the new user
+    const newUser = await User.create({
+      email,
+      username,
+      password: hashedPassword,
+      role,
+    });
 
-      // Hash the password
-      const hashedPassword = await hashPassword(password);
+    const token = createToken({
+      email: newUser.email,
+      username: newUser.username,
+    });
 
-      // Create the new user
-      const newUser = await User.create({
-        email,
-        username,
-        password: hashedPassword,  
-        role,
-      });
+    return res
+      .status(201)
+      .json({ message: "User created successfully!", token });
+  } catch (error) {
+    console.error(error);
 
-      const token = createToken({ email: newUser.email, username: newUser.username });
-
-      return res.status(201).json({ message: "User created successfully!", token });
-    } catch (error) {
-      console.error(error);
-
-      return res.status(500).json({ message: "Server error. Please try again later." })
-    }
-  } else {
-    
-    return res.status(405).json({ message: "Method not allowed" });
+    return res
+      .status(500)
+      .json({ message: "Server error. Please try again later." });
   }
 };
 
